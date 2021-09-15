1e89da5efc21fb2095e8a77ef7a1908701e30991
Tijani Fulani <Fulani07@gmail.com>
2021-09-15 12:24:05 +0100

    feat: created header for landing.jsx

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,5 +1,19 @@
 // import React from 'react'
 
-const Landing = () => {};
+const Landing = () => {
+  return (
+    <header>
+      <div className='header-left'>
+        <h1 className='logo'>
+          CoupleFinance<span>.</span>
+        </h1>
+      </div>
+      <div className='header-btns'>
+        <button className='btn btn-primary'>Signup</button>
+        <button className='btn btn-primary'>Login</button>
+      </div>
+    </header>
+  );
+};
 
 export default Landing;
