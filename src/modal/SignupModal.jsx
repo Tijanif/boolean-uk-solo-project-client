03cdf16b509a057e4203e456de5765a5e34b4535
Tijani Fulani <Fulani07@gmail.com>
2021-09-16 13:34:29 +0100

    Build: sign up modal with data in console

--- src/modal/SignupModal.jsx
+++ src/modal/SignupModal.jsx
@@ -1,8 +1,24 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import useStore from '../store';
 
 function SignupModal(props) {
+  const setSignupUser = useStore((state) => state.setSignupUser);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const target = e.target;
+
+    const newUser = {
+      name: target.name.value,
+      email: target.email.value,
+      password: target.password.value,
+    };
+    console.log(newUser);
+    setSignupUser(newUser);
+  };
   return (
     <Modal
       {...props}
@@ -14,14 +30,14 @@ function SignupModal(props) {
         <Modal.Title id='contained-modal-title-vcenter'>Sign Up</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className='mb-3' controlId='formBasicName'>
             <Form.Label>Name</Form.Label>
-            <Form.Control type='name' placeholder='Name' />
+            <Form.Control type='name' name='name' placeholder='Name' />
           </Form.Group>
           <Form.Group className='mb-3' controlId='formBasicEmail'>
             <Form.Label>Email address</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' />
+            <Form.Control type='email' name='email' placeholder='Enter email' />
             <Form.Text className='text-muted'>
               We'll never share your email with anyone else.
             </Form.Text>
@@ -29,7 +45,11 @@ function SignupModal(props) {
 
           <Form.Group className='mb-3' controlId='formBasicPassword'>
             <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' />
+            <Form.Control
+              type='password'
+              name='password'
+              placeholder='Password'
+            />
           </Form.Group>
           <Button variant='primary' type='submit'>
             Submit
--- src/store.js
+++ src/store.js
@@ -2,8 +2,33 @@ import React from 'react';
 import create from 'zustand';
 
 const useStore = create((set, get) => ({
-  modal: '',
-  setModal: (modal) => set({ modal }),
+  signUpUserCredentials: {
+    name: null,
+    email: null,
+    password: null,
+  },
+  setSignUpUserCredentials: (signUpUserCredentials) =>
+    set({ signUpUserCredentials }),
+  signedUpUser: null,
+  setSignupUser: async (signUpUserCredentials) => {
+    const signupUser = await fetch(`http://localhost:3030/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(signUpUserCredentials),
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    });
+    if (signupUser) set({ loggedInUser: signupUser });
+  },
+  logOut: () => {
+    set({
+      loggedInUser: null,
+    });
+  },
 }));
 
 export default useStore;
